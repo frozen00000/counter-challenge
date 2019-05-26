29913a9178c13cbaedd0d202fc46eda3b8c66ffc
timurlevadny <frozen00000>
2019-05-26 13:52:42 -0700

    changed post method to get because of benchmark tool

--- src/main/java/com/frozen/counterchallenge/CounterChallengeApplication.java
+++ src/main/java/com/frozen/counterchallenge/CounterChallengeApplication.java
@@ -3,7 +3,7 @@ package com.frozen.counterchallenge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -11,22 +11,23 @@ import java.util.concurrent.atomic.LongAccumulator;
 
 @SpringBootApplication
 @RestController
+@ResponseBody
 public class CounterChallengeApplication {
 
-	private LongAccumulator atomicLong = new LongAccumulator(Long::sum, 0);
+	private final LongAccumulator atomicLong = new LongAccumulator(Long::sum, 0);
 
 	public static void main(String[] args) {
 		SpringApplication.run(CounterChallengeApplication.class, args);
 	}
 
-	@PostMapping
-	public void inc() {
-		atomicLong.accumulate(1);
-	}
-
-	@GetMapping
+	@GetMapping("/count")
 	public Mono<Long> get() {
 		return Mono.just(atomicLong.get());
+	}
+
+	@GetMapping("/")
+	public void inc() {
+		atomicLong.accumulate(1);
 	}
 
 }
